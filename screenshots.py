f33d646f0d17547ebb739a4d0d086951e599b523
Vaibhav Jha <vaibhavkumar2012@gmail.com>
2024-06-04 08:24:03 +0400

    Update screenshots.py
    Added the function to add the URL Bar in the screenshot and save the file with the URL name.

--- screenshots.py
+++ screenshots.py
@@ -1,18 +1,22 @@
 import time
+import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pyautogui
 
 
-def take_screenshot(url, output_file):
+def take_screenshot_with_url_bar(url, output_file):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # To run Chrome in headless mode (no GUI)
     chrome_options.add_argument("--start-maximized")  # Maximize window
     driver = webdriver.Chrome(options=chrome_options)
 
     try:
         driver.get(url)
         time.sleep(5)  # Adjust the sleep duration as needed (e.g., 5 seconds)
-        driver.save_screenshot(output_file)
+
+        # Take screenshot of the entire screen
+        screenshot = pyautogui.screenshot()
+        screenshot.save(output_file)
         print(f"Screenshot saved: {output_file}")
     except Exception as e:
         print(f"Failed to take screenshot: {e}")
@@ -20,11 +24,19 @@ def take_screenshot(url, output_file):
         driver.quit()
 
 
-if __name__ == "__main__":
-    input_file = "urls.txt"
+def main():
+    input_file = "domains.txt"
+
     with open(input_file, "r") as file:
-        urls = file.readlines()
-    for i, url in enumerate(urls):
-        url = url.strip()  # Remove leading/trailing whitespace and newline characters
-        output_file = f"screenshot_{i}.png"
-        take_screenshot(url, output_file)
+        domains = [line.strip() for line in file]
+
+    for domain in domains:
+        url = f"http://{domain}"
+        # Replace special characters in domain name to make it a valid filename
+        sanitized_domain = domain.replace(".", "_").replace("/", "_")
+        output_screenshot_file = f"{sanitized_domain}.png"
+        take_screenshot_with_url_bar(url, output_screenshot_file)
+
+
+if __name__ == "__main__":
+    main()
